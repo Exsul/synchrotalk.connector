75ecf109f6b0f5411486d505a7248bb1ee483c09
Kirill Berezin <enelar@exsul.net>
2016-04-23 20:13:07 +0300

    Fix missing function keyword

--- connector.php
+++ connector.php
@@ -4,15 +4,15 @@ require_once('synchrotalk.plugin/plugin.php');
 
 class connector implements plugin
 {
-  abstract public log_in($username, $password);
-  abstract public sign_in($token);
+  abstract public function log_in($username, $password);
+  abstract public function sign_in($token);
 
 
-  abstract public send_message($to, $what);
-  abstract public fetch_messages($thread);
-  abstract public mark_read($thread);
+  abstract public function send_message($to, $what);
+  abstract public function fetch_messages($thread);
+  abstract public function mark_read($thread);
 
 
-  abstract public threads();
-  abstract public contacts();
+  abstract public function threads();
+  abstract public function contacts();
 }
