725c64cade115b6aa82d8ad88f4b9bd1f3ce2d70
Kirill Berezin <enelar@exsul.net>
2016-08-04 07:25:39 +0300

    Make each method optional, add redirect authorisation

--- auth.php
+++ auth.php
@@ -1,4 +1,4 @@
-<?php namespace synchrotalk\connector
+<?php namespace synchrotalk\connector;
 
 require_once('synchrotalk.plugin/plugin.php');
 
@@ -6,12 +6,17 @@ abstract class auth implements plugin
 {
   abstract public /* user */ function sign_in( /* string */ $token);
 
-  abstract public /* token */ function direct_auth();
+  public /* string[] */ function direct_auth_requirments();
+  public /* token */ function direct_auth($requirments);
 
-  abstract public /* string[] */ function callback_auth_requirments();
-  abstract public /* string[] */ function callback_auth_question(/* string[] */ $requirments);
-  abstract public /* token */ function callback_auth_answer(/* string[] */ $requirments);
+  public /* string[] */ function callback_auth_requirments();
+  public /* string[] */ function callback_auth_question(/* string[] */ $requirments);
+  public /* token */ function callback_auth_answer(/* string[] */ $requirments);
 
-  abstract public /* string[] */ function supported_authtypes();
+  public /* string[] */ function redirect_auth_requirments();
+  public /* string */ function redirect_auth_question($requirments);
+  public /* token */ function redirect_auth_answer(/* string[] */ $requirments);
+
+  public /* string[] */ function supported_authtypes();
   abstract public /* string */ function preferred_authtype();
 }
