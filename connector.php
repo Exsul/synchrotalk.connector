2bc0bb226d5547644764ea137dd98d8844653486
Kirill Berezin <enelar@exsul.net>
2016-08-04 05:18:46 +0300

    Refactor connector interface, make it independed of the way authorisation was made

--- connector.php
+++ connector.php
@@ -1,20 +1,20 @@
-<?php
+<?php namespace synchrotalk\connector
 
 require_once('synchrotalk.plugin/plugin.php');
 
 abstract class connector implements plugin
 {
-  abstract public /* user */ function log_in( /* string */ $username, /* string */ $password);
-  abstract public /* user */ function sign_in( /* string */ $token);
+  public function __construct(/* auth or token */ $auth);
 
-
-  abstract public /* object */ function send_message(/* thread_id */ $to, /* string or message */ $what);
-  abstract public /* thread */ function userid_to_thread( /* string */ $user_id );
-
-  abstract public /* message[] */ function fetch_messages( /* string */ $thread_id, /* int */ $skip_pages = 0);
-  abstract public /* object */ function mark_read( /* string */ $thread_id);
-
-
-  abstract public /* threads[] */ function threads();
+  abstract public /* notification[] */ function notifications();
   abstract public /* users[] */ function contacts();
+  abstract public /* threads[] */ function threads();
+  abstract public /* message[] */ function messages( /* string */ $thread_id, /* int */ $skip_pages = 0);
+
+
+  abstract public /* object */ function message_mark_read( /* string */ $thread_id);
+  abstract public /* object */ function message_send(/* thread_id */ $to, /* string or message */ $what);
+
+
+  abstract public /* thread */ function userid_to_thread( /* string */ $user_id );
 }
